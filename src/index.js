bcd9ebf9a398d3d2fbd544218192f10917e2cdb9
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-05-01 01:06:50 +0200

    Fixed termination propagation

--- src/index.js
+++ src/index.js
@@ -1,45 +1,58 @@
-const noop = () => {}
-
 export default function exhaustMap(project) {
   return source => (start, sink) => {
     if (start !== 0) return
 
-    let projected = null
-    let completed = false
+    let innerTalkback = null
+    let sourceTalkback
 
-    source(0, (type, data) => {
+    const talkback = (type, data) => {
       if (type === 0) return
 
+      sourceTalkback(type, data)
+
+      if (type === 2 && innerTalkback !== null) {
+        innerTalkback(type, data)
+      }
+    }
+
+    const innerCallbag = (type, data) => {
+      if (type === 0) {
+        innerTalkback = data
+        sink(0, talkback)
+        return
+      }
+
       if (type === 1) {
-        if (projected !== null) return
-        projected = project(data)
-
-        projected(0, (type, data) => {
-          if (type === 0) {
-            sink(0, noop)
-            return
-          }
-
-          if (type === 1) {
-            sink(1, data)
-            return
-          }
-
-          if (type === 2) {
-            projected = null
-            if (completed) {
-              sink(2)
-            }
-          }
-        })
+        sink(1, data)
         return
       }
 
       if (type === 2) {
-        completed = true
-        if (projected === null || data !== undefined) {
-          sink(2, data)
+        innerTalkback = null
+      }
+    }
+
+    source(0, (type, data) => {
+      if (type === 0) {
+        sourceTalkback = data
+        return
+      }
+
+      if (type === 1) {
+        if (innerTalkback !== null) return
+
+        project(data)(0, innerCallbag)
+        return
+      }
+
+      if (type === 2) {
+        sink(2, data)
+
+        if (innerTalkback === null) {
+          return
         }
+
+        innerTalkback(2, data)
       }
     })
   }
